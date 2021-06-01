44aea72760805990496aee440c9803a73ad0717f
TearX <froomexplay@gmail.com>
2021-06-01 18:42:52 +0200

    v1.1
    Villagers now can be brought back to awareness whit a single punch.

--- src/me/tear/villager/VillagerEntity.java
+++ src/me/tear/villager/VillagerEntity.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
 
 public class VillagerEntity implements Listener {
@@ -27,8 +28,25 @@ public class VillagerEntity implements Listener {
 			}
 				e.setCancelled(true);
 				return;
-		}
-		
+		}	
 	}
 	
+	@EventHandler
+	public void onDamage(EntityDamageByEntityEvent e) {
+		
+		Entity entity = e.getEntity();
+		
+		if (entity.getType() == EntityType.VILLAGER) {
+			
+			CraftVillager v = (CraftVillager) entity;
+			
+			if (v.isAware()) {
+				return;
+			}
+			
+			v.setAware(true);
+			e.setCancelled(true);
+			return;
+		}
+	}
 }
